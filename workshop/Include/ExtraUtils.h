c4db752b57ea1d57a74fa3adeead99427c68fe76
VTrider <157431961+VTrider@users.noreply.github.com>
2026-04-12 17:55:43 -0400

    updated workshop release

--- workshop/Include/ExtraUtils.h
+++ workshop/Include/ExtraUtils.h
@@ -10,29 +10,31 @@
 
 #include <Windows.h>
 #include <delayimp.h>
+#undef CONST
 
 #include <cstdint>
 #include <filesystem>
 #include <format>
+#include <string>
 
 namespace exu2
 {
 #ifndef EXU_EXPORTS
-	std::filesystem::path GetWorkshopPath();
+	const std::filesystem::path GetWorkshopPath();
 
 	// WARNING: You MUST call these two functions in DLL_PROCESS_ATTACH, and DLL_PROCESS_DETACH
 	// respectively if you are using this library in a dll mission. By default this uses the 
 	// version of the library currently on the steam workshop. If you are using a custom or
 	// development build, you must set the dll directory accordingly. See the README on
 	// GitHub for more info.
-	inline void ProcessAttach()
+	inline void ProcessAttach(const std::filesystem::path& dllDirectory = GetWorkshopPath() / "3515140097" / "Bin")
 	{
 		SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_APPLICATION_DIR |
 								 LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | 
 								 LOAD_LIBRARY_SEARCH_SYSTEM32 |
 								 LOAD_LIBRARY_SEARCH_USER_DIRS
         );
-        AddDllDirectory(GetWorkshopPath().append("3515140097").append("Bin").c_str());
+		AddDllDirectory(dllDirectory.wstring().c_str());
 	}
 
 	inline void ProcessDetach()
@@ -45,18 +47,23 @@ namespace exu2
 
 	// Use this to compare against the DLL version. You should make sure that
 	// your header is up to date with the latest DLL.
-	constexpr const char* HEADER_VERSION = "1.3.0";
+	constexpr const char* HEADER_VERSION = "1.4.0";
 #else
-	EXUAPI int DLLAPI GetGameMinorVersion();
 	constexpr int MINIMUM_REQUIRED_VERSION = 185;
 #endif
+
+	// Returns the minor version of the game, use this if your mod only supports a certain version(s)
+	EXUAPI int DLLAPI GetGameMinorVersion();
+	
+	// Returns the patch version of the game
+	EXUAPI int DLLAPI GetGamePatchVersion();
 
 	// Returns the current dll version Major.Minor.Patch
 	EXUAPI const char* DLLAPI GetDLLVersion();
 
 	using VarSysHandler = void(__cdecl*)(unsigned long crc);
 
-	struct iVector2
+	struct IVector2
 	{
 		int x, y;
 	};
@@ -99,46 +106,35 @@ namespace exu2
 	// be a minimum of 1, subtract 1 for the real count of arguments supplied
 	EXUAPI int DLLAPI IFace_GetArgCount();
 
-	// The out parameter in these three functions should not be a nullptr,
-	// return value is true if it could parse the correct data type, false if not.
+	// Return value is true if it could parse the correct data type, false if not.
+	// The out parameter will not be modified if the function returns false.
 	// In C++ you should initialize the value to 0 or nullptr in the case of GetArgString,
 	// in Lua it will always return nil if the function fails
 
 	// Gets a float value at the given position if it exists
-	EXUAPI bool DLLAPI IFace_GetArgFloat(int arg, float* value);
+	EXUAPI bool DLLAPI IFace_GetArgFloat(int arg, float& value);
 
 	// Gets an integer value at the given position if it exists
-	EXUAPI bool DLLAPI IFace_GetArgInteger(int arg, int* value);
+	EXUAPI bool DLLAPI IFace_GetArgInteger(int arg, int& value);
 
 	// Gets a string value at the given position if it exists
-	EXUAPI bool DLLAPI IFace_GetArgString(int arg, char** value);
+	EXUAPI bool DLLAPI IFace_GetArgString(int arg, char*& value);
+
+	// Gets a bool value at the given position if it exists.
+	// Accepts "true" or "false", or,  0 or 1
+	EXUAPI bool DLLAPI IFace_GetArgBool(int arg, bool& value);
 
 	// Filesystem
 
-	inline std::filesystem::path GetBZCCPath()
-	{
-		wchar_t bzccPath[MAX_PATH];
-		GetModuleFileNameW(NULL, bzccPath, MAX_PATH);
-		return std::filesystem::path(bzccPath).parent_path();
-	}
-
-	inline std::filesystem::path GetWorkshopPath()
-	{
-		DWORD size = MAX_PATH;
-		std::wstring steamPath(MAX_PATH, L'\0');
-		RegGetValueW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node\\Valve\\Steam", L"InstallPath", RRF_RT_REG_SZ, NULL, steamPath.data(), &size);
-
-		steamPath.resize(size / sizeof(wchar_t) - 1);
-		std::filesystem::path workshopPath(steamPath);
-		workshopPath.append("steamapps").append("workshop").append("content").append("624970");
-
-		return workshopPath;
-	}
+	// Helpers for common directories that mods usually use
+	const std::filesystem::path GetBZCCPath();
+	const std::filesystem::path GetMyDocs();
+	const std::filesystem::path GetWorkshopPath();
 
 	// Graphics
 
 	// Gets the current viewport size in pixels (X, Y)
-	EXUAPI iVector2 GetViewportSize();
+	EXUAPI IVector2 GetViewportSize();
 
 	// Mission
 
@@ -160,12 +156,28 @@ namespace exu2
 	// Returns 0 if there is no player associated with the team.
 	EXUAPI uint64_t DLLAPI GetSteam64(int team);
 
+	// Terrain
+
+	enum class TerrainQueryResult : uint32_t
+	{
+		NOT_BUILDABLE,
+		BUILDABLE,
+		INVALID_ODF
+	};
+
+	// Queries if the tile closest to `pos` is valid for building the given `team` and `odf`. `front` is the direction
+	// the building is facing. BUILDABLE if the tile is buildable and fills the out parameter `pos` with
+	// the resulting build matrix position. Returns NOT_BUILDABLE if the tile is not buildable and `pos` is unmodified.
+	// Returns INVALID_ODF if the odf does not have a GameObjectClass associated with it (probably malformed in some way)
+	// IMPORTANT NOTE: if you don't want the game to stutter the first time you call this function for a given odf,
+	// make sure to call PreloadODF(odf) in InitialSetup/Start or wherever you want beforehand.
+	EXUAPI TerrainQueryResult DLLAPI IsTerrainBuildable(int team, const char* odf, Vector& pos, const Vector& front);
+
 	// VarSys
 
 	// Low level create command. Creates an "unregistered" VarSys command, it will show up in the `ls` command but
 	// it will not seen by the current mission's ProcessCommand function. You can use VarSys_RegisterHandler to set
-	// a custom handler that can even work outside of a game. WARNING: these commands are NODELETE by default
-	// and I don't know why, so this function probably isn't too useful. UNABLE TO BE BOUND IN LUA
+	// a custom handler that can even work outside of a game. IFace_CreateCommand should be used when in a normal mission.
 	EXUAPI void DLLAPI VarSys_CreateCmd(ConstName name);
 
 	// Deletes an IFace item and all its subdirectories, returns true if
@@ -177,6 +189,25 @@ namespace exu2
 	// The magic number is usually 0 in the game's code so I recommend passing that in, anything else is untested.
 	// UNABLE TO BE BOUND IN LUA
 	EXUAPI void DLLAPI VarSys_RegisterHandler(ConstName name, VarSysHandler handler, unsigned long magic);
+
+	// Installs a global command handler. This handles anything in the "global scope" like "ls"
+	// or "help"
+	EXUAPI void DLLAPI VarSys_InstallGlobalHandler(VarSysHandler handler);
+
+	// Uninstalls the installed global handler if it exists
+	EXUAPI void DLLAPI VarSys_UninstallGlobalHandler();
+
+	enum class VarFlag : uint32_t {
+		CONST = 0x4,
+		NODELETE = 0x8000
+	};
+
+	// Checks if a flag is set on an IFace variable/VarItem and stores the status in `flagStatus`.
+	// Returns true if the var exists, false otherwise.
+	EXUAPI bool DLLAPI VarSys_GetVarFlag(ConstName name, VarFlag flag, bool& flagStatus);
+
+	// Sets a flag on an IFace variable/VarItem. Returns true if the var exists, false otherwise.
+	EXUAPI bool DLLAPI VarSys_SetVarFlag(ConstName name, VarFlag flag, bool status);
 
 	// DLL Only Helpers
 
@@ -193,6 +224,37 @@ namespace exu2
 	}
 
 	// Do not modify
+
+	// These are inline since it's risky to pass an stl object across dll boundaries
+
+	inline const std::filesystem::path GetBZCCPath()
+	{
+		wchar_t bzccPath[MAX_PATH];
+		GetModuleFileNameW(NULL, bzccPath, MAX_PATH);
+		return std::filesystem::path(bzccPath).parent_path();
+	}
+
+	inline const std::filesystem::path GetMyDocs()
+	{
+		size_t bufSize = 0;
+		GetOutputPath(bufSize, nullptr);
+		std::unique_ptr<wchar_t[]> path = std::make_unique<wchar_t[]>(bufSize);
+		GetOutputPath(bufSize, path.get());
+		return std::filesystem::path(path.get());
+	}
+
+	inline const std::filesystem::path GetWorkshopPath()
+	{
+		DWORD size = MAX_PATH;
+		std::wstring steamPath(MAX_PATH, L'\0');
+		RegGetValueW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node\\Valve\\Steam", L"InstallPath", RRF_RT_REG_SZ, NULL, steamPath.data(), &size);
+
+		steamPath.resize(size / sizeof(wchar_t) - 1);
+		std::filesystem::path workshopPath(steamPath);
+		workshopPath.append("steamapps").append("workshop").append("content").append("624970");
+
+		return workshopPath;
+	}
 
 #ifndef EXU_EXPORTS
 	inline FARPROC WINAPI DelayLoadHandler(unsigned int dliNotify, [[maybe_unused]] PDelayLoadInfo pdli)
@@ -217,4 +279,32 @@ namespace exu2
 	extern "C" __declspec(selectany) const PfnDliHook  __pfnDliNotifyHook2 = DelayLoadHandler;
 	extern "C" __declspec(selectany) const PfnDliHook  __pfnDliFailureHook2 = DelayLoadHandler;
 #endif
+}
+
+namespace std
+{
+	using namespace exu2;
+
+	template <>
+	struct formatter<TerrainQueryResult>: formatter<string>
+	{
+		auto format(TerrainQueryResult r, format_context& ctx) const
+		{
+			using enum TerrainQueryResult;
+			std::string str;
+			switch (r)
+			{
+			case NOT_BUILDABLE:
+				str = "NOT_BUILDABLE";
+				break;
+			case BUILDABLE:
+				str = "BUILDABLE";
+				break;
+			case INVALID_ODF:
+				str = "INVALID_ODF";
+				break;
+			}
+			return format_to(ctx.out(), "{}", str);
+		}
+	};
 }
